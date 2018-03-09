fd32edc2c576b8a64b40861cc48efa444a6d3365
Steve <steve21johnson@gmail.com>
2018-03-09 15:37:04 -0500

    work + rest inputs!

--- pomo.js
+++ pomo.js
@@ -1,32 +1,37 @@
 $(document).ready(function() {
-	var secs = $('input').val().split(":")[1];
-	var mins = $('input').val().split(":")[0];
-	console.log(mins +":"+ secs)
-	// var iT = secs + mins*60; //65
-	secs = eval(secs)
-	var iT = eval( mins*60 +secs)
-	console.log(iT);
-
 		$("#start").click(function() {
+			var secs = eval($('#work-input').val().split(":")[1]);
+			var mins = eval($('#work-input').val().split(":")[0]);
+			// secs = eval($('#rest-input').val().split(":")[1]);
+			// mins = eval($('#rest-input').val().split(":")[0]);
+			var iT = eval( mins*60 + secs);
 			var int = setInterval(tick, 1000);
 			$("#stop").click(function() {
 				clearInterval(int);
-		});
+			});
 
+	//switch from rest - work
 		function tick() {
 			if (mins == 0 && secs == 0) { //0:0
 				$('#work').toggle();
 				$('#rest').toggle();
 				var color = $('#circle').css('background-color')
+//if color red, switch work to rest
 				if (color=='rgb(255, 0, 0)') {
 					$('#circle').css('background-color','green');
+					 secs = eval($('#rest-input').val().split(":")[1]);
+					 mins = eval($('#rest-input').val().split(":")[0]);
+					 iT = eval( mins*60 + secs)
 				}
+	//if color green, switch rest to work
 				else {
 					$('#circle').css('background-color','red');
+					secs = eval($('#work-input').val().split(":")[1]);
+					mins = eval($('#work-input').val().split(":")[0]);
+					iT = eval( mins*60 + secs)
 				}
-				// secs =  3;
-				// mins = 0;
 
+//decrement timer values
 			}
 			else if (mins !== 0 && secs == 0) { //1:00
 				mins=mins-1;
@@ -35,6 +40,7 @@ $(document).ready(function() {
 			else if ((mins !== 0||mins==0) && secs !== 0) { //1:01
 				secs=secs-1;
 			}
+//correct timer display
 			if (secs<10){
 				$('#timer-display').text(mins + ":0" + secs);
 
@@ -42,9 +48,10 @@ $(document).ready(function() {
 			else {
 				$('#timer-display').text(mins + ":" + secs);
 			}
+//math for adjustment in cover
 			var cT = secs + mins*60;
-			var frac = cT/iT;
-			// console.log(frac);
+			var frac = cT / iT;
+			//move cover every cyccle
 			$('#cover').css("height",frac*20 + "em");
 		}
 
