40085f20443a30469178e9b4770495241f4ef1e5
Steve <steve21johnson@gmail.com>
2018-03-09 14:39:50 -0500

    input timer working

--- pomo.js
+++ pomo.js
@@ -1,32 +1,32 @@
 $(document).ready(function() {
-	$("#start").click(function() {
-		var secs = 3;
-		var mins = 0;
-		var iT = secs + mins*60; //65
-		// console.log(iT);
-		var int = setInterval(tick, 1000);
-		$("#stop").click(function() {
-			clearInterval(int);
-		});
+	var secs = $('input').val().split(":")[1];
+	var mins = $('input').val().split(":")[0];
+	console.log(mins +":"+ secs)
+	// var iT = secs + mins*60; //65
+	secs = eval(secs)
+	var iT = eval( mins*60 +secs)
+	console.log(iT);
 
+		$("#start").click(function() {
+			var int = setInterval(tick, 1000);
+			$("#stop").click(function() {
+				clearInterval(int);
+		});
 
 		function tick() {
 			if (mins == 0 && secs == 0) { //0:0
-				console.log('buzz');
 				$('#work').toggle();
 				$('#rest').toggle();
 				var color = $('#circle').css('background-color')
-				console.log(color);
-				console.log(color=='rgb(255, 0, 0)')
 				if (color=='rgb(255, 0, 0)') {
 					$('#circle').css('background-color','green');
-					console.log($('#circle').css('background-color'))
 				}
 				else {
 					$('#circle').css('background-color','red');
 				}
-				secs =  3;
-				mins = 0;
+				// secs =  3;
+				// mins = 0;
+
 			}
 			else if (mins !== 0 && secs == 0) { //1:00
 				mins=mins-1;
