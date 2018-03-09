34710411462be569b312053f6daea4cb871b17d3
Steve <steve21johnson@gmail.com>
2018-03-09 13:58:10 -0500

    fixed timer, but functioning

--- pomo.js
+++ pomo.js
@@ -1,18 +1,32 @@
 $(document).ready(function() {
 	$("#start").click(function() {
-		var secs = 5;
-		var mins = 1;
+		var secs = 3;
+		var mins = 0;
 		var iT = secs + mins*60; //65
-		console.log(iT);
+		// console.log(iT);
 		var int = setInterval(tick, 1000);
 		$("#stop").click(function() {
 			clearInterval(int);
 		});
-		
+
 
 		function tick() {
 			if (mins == 0 && secs == 0) { //0:0
-				alert ('buzz');
+				console.log('buzz');
+				$('#work').toggle();
+				$('#rest').toggle();
+				var color = $('#circle').css('background-color')
+				console.log(color);
+				console.log(color=='rgb(255, 0, 0)')
+				if (color=='rgb(255, 0, 0)') {
+					$('#circle').css('background-color','green');
+					console.log($('#circle').css('background-color'))
+				}
+				else {
+					$('#circle').css('background-color','red');
+				}
+				secs =  3;
+				mins = 0;
 			}
 			else if (mins !== 0 && secs == 0) { //1:00
 				mins=mins-1;
@@ -21,12 +35,17 @@ $(document).ready(function() {
 			else if ((mins !== 0||mins==0) && secs !== 0) { //1:01
 				secs=secs-1;
 			}
-			$('#timer-display').text(mins + ":" + secs);
+			if (secs<10){
+				$('#timer-display').text(mins + ":0" + secs);
+
+			}
+			else {
+				$('#timer-display').text(mins + ":" + secs);
+			}
 			var cT = secs + mins*60;
 			var frac = cT/iT;
-			console.log(frac);
+			// console.log(frac);
 			$('#cover').css("height",frac*20 + "em");
-			$('#status-display').text(frac);
 		}
 
 	});
